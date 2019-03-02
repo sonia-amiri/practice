254cb43eb6b62faed6d34cad285a73f5f67e3687
sonia <amirisomayeh74@gmail.com>
2019-03-02 04:08:54 -0800

    second example

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ let {name,years:age,isAdmin=false} = user;
 console.log(name);
 console.log(age);
 console.log(isAdmin);
+
+
+
+function topSalary(salaries) {
+
+    let max = 0;
+    let maxName = null;
+  
+    for(let [name, salary] of Object.entries(salaries)) {
+      if (max < salary) {
+        max = salary;
+        maxName = name;
+      }
+    }
+  
+    return maxName;
+  }
